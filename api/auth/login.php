e005d6bbf4cd8963e68264ecb978f4557f2d7c36
Walter Emmanuel Benitez <walter.emmanuel.benitez@gmail.com>
2025-06-09 11:58:22 -0300

    Intentando hacer merge correctamente

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,8 +1,14 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
+header("Access-Control-Allow-Origin: http://localhost:3001*"); // o "*" si es solo para desarrollo
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 
 // Incluir la conexión a la base de datos
 require_once '../db.php';
